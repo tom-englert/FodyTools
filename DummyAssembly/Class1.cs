92d624364fce28f97ce745871eb5203632421eb1
tom-englert <mail@tom-englert.de>
2018-08-11 19:04:15 +0200

    Move Extension methods to retrieve system references from autoproperties.fody

---
+++ DummyAssembly/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace DummyAssembly
+{
+    public class Class1
+    {
+        private int field;
+
+        public void Method(int param)
+        {
+            field = param;
+            param += 1;
+            if (param == field)
+            {
+                field -= 1;
+            }
+        }
+    }
+}
--- FodyTools.Tests/CodeImporterTests.cs
+++ FodyTools.Tests/CodeImporterTests.cs
@@ -1,4 +1,4 @@
-﻿namespace Tests
+﻿namespace FodyTools.Tests
 {
     using System;
     using System.Collections;
