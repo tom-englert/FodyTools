29d34668f0edd27e3fd40ef43f5a737fe4ad85d1
tom-englert <mail@tom-englert.de>
2018-10-10 10:29:40 +0200

    Add default ILogger implementation

--- FodyTools/ILogger.cs
+++ FodyTools/ILogger.cs
@@ -1,7 +1,10 @@
 ﻿namespace FodyTools
 {
-    using Mono.Cecil.Cil;
+    using System.Diagnostics.CodeAnalysis;
 
+    using Fody;
+
+    using Mono.Cecil.Cil;
 
     /// <summary>
     /// A generic logger interface to decouple implementation
@@ -12,5 +15,43 @@
         void LogInfo(string message);
         void LogWarning(string message, SequencePoint sequencePoint = null);
         void LogError(string message, SequencePoint sequencePoint = null);
+    }
+
+    [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+    public abstract class LoggingBaseModuleWeaver : BaseModuleWeaver, ILogger
+    {
+        void ILogger.LogDebug(string message)
+        {
+            LogDebug(message);
+        }
+
+        void ILogger.LogInfo(string message)
+        {
+            LogInfo(message);
+        }
+
+        void ILogger.LogWarning(string message, SequencePoint sequencePoint)
+        {
+            if (sequencePoint == null)
+            {
+                LogWarning(message);
+            }
+            else
+            {
+                LogWarningPoint(message, sequencePoint);
+            }
+        }
+
+        void ILogger.LogError(string message, SequencePoint sequencePoint)
+        {
+            if (sequencePoint == null)
+            {
+                LogError(message);
+            }
+            else
+            {
+                LogErrorPoint(message, sequencePoint);
+            }
+        }
     }
 }
