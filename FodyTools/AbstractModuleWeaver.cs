18619a71c8e31019ffa65b41a013014a76d8c77e
tom-englert <mail@tom-englert.de>
2018-10-28 18:46:14 +0100

    Add some documentation

--- FodyTools/AbstractModuleWeaver.cs
+++ FodyTools/AbstractModuleWeaver.cs
@@ -12,65 +12,145 @@
     using Mono.Cecil.Cil;
 
     /// <summary>
-    /// A generic logger interface to decouple implementation
+    /// A generic logger interface to decouple implementation.
     /// </summary>
     public interface ILogger
     {
-        void LogDebug(string message);
-        void LogInfo(string message);
-        void LogWarning(string message, SequencePoint sequencePoint = null);
-        void LogError(string message, SequencePoint sequencePoint = null);
+        /// <summary>
+        /// Logs a debug message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void LogDebug([NotNull] string message);
+        /// <summary>
+        /// Logs an info message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void LogInfo([NotNull] string message);
+        /// <summary>
+        /// Logs a warning.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="sequencePoint">The optional sequence point where the problem occurred.</param>
+        void LogWarning([NotNull] string message, SequencePoint sequencePoint = null);
+        /// <summary>
+        /// Logs an error.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="sequencePoint">The optional sequence point where the error occurred.</param>
+        void LogError([NotNull] string message, SequencePoint sequencePoint = null);
     }
 
+    /// <summary>
+    /// A generic type system interface to decouple implementation.
+    /// </summary>
     public interface ITypeSystem
     {
-        TypeDefinition FindType(string typeName);
-        bool TryFindType(string typeName, out TypeDefinition value);
+        /// <summary>
+        /// Finds the type in the assemblies that the weaver has registered for scanning.
+        /// </summary>
+        /// <param name="typeName">Name of the type.</param>
+        /// <returns>The type definition</returns>
+        [NotNull]
+        TypeDefinition FindType([NotNull] string typeName);
+
+        /// <summary>
+        /// Finds the type in the assemblies that the weaver has registered for scanning.
+        /// </summary>
+        /// <param name="typeName">Name of the type.</param>
+        /// <param name="value">The return value.</param>
+        /// <returns><c>true</c> if the type was found and value contains a valid item.</returns>
+        [ContractAnnotation("value:null => false")]
+        bool TryFindType([NotNull] string typeName, out TypeDefinition value);
     }
 
+    /// <summary>
+    /// A <see cref="Fody.BaseModuleWeaver" /> implementing <see cref="ILogger"/> and <see cref="ITypeSystem"/>, decorated with <see cref="NotNullAttribute"/>.
+    /// </summary>
+    /// <seealso cref="Fody.BaseModuleWeaver" />
+    /// <seealso cref="FodyTools.ILogger" />
+    /// <seealso cref="FodyTools.ITypeSystem" />
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
     [ExcludeFromCodeCoverage]
     public abstract class AbstractModuleWeaver : BaseModuleWeaver, ILogger, ITypeSystem
     {
+        /// <summary>
+        /// An instance of <see cref="T:Mono.Cecil.ModuleDefinition" /> for processing.
+        /// </summary>
         [NotNull]
         protected new ModuleDefinition ModuleDefinition => base.ModuleDefinition;
 
+        /// <summary>
+        /// The full element XML from FodyWeavers.xml.
+        /// </summary>
         [NotNull]
         protected new XElement Config => base.Config;
 
+        /// <summary>
+        /// Commonly used <see cref="T:Mono.Cecil.TypeReference" />s.
+        /// </summary>
         [NotNull]
         protected new Fody.TypeSystem TypeSystem => base.TypeSystem;
 
+        /// <summary>
+        /// The full path of the target assembly.
+        /// </summary>
         [NotNull]
         protected new string AssemblyFilePath => base.AssemblyFilePath;
 
+        /// <summary>
+        /// The full directory path of the target project.
+        /// A copy of $(ProjectDir).
+        /// </summary>
         [NotNull]
         protected new string ProjectDirectoryPath => base.ProjectDirectoryPath;
 
+        /// <summary>
+        /// The full directory path of the current weaver.
+        /// </summary>
         [NotNull]
         protected new string AddinDirectoryPath => base.AddinDirectoryPath;
 
+        /// <summary>
+        /// The full directory path of the current solution.
+        /// A copy of `$(SolutionDir)` or, if it does not exist, a copy of `$(MSBuildProjectDirectory)..\..\..\`. OPTIONAL
+        /// </summary>
         [NotNull]
         protected new string SolutionDirectoryPath => base.SolutionDirectoryPath;
 
+        /// <summary>
+        /// A semicolon delimited string that contains
+        /// all the references for the target project.
+        /// A copy of the contents of the @(ReferencePath).
+        /// </summary>
         [NotNull]
         protected new string References => base.References;
 
+        /// <summary>
+        /// A list of all the references marked as copy-local.
+        /// A copy of the contents of the @(ReferenceCopyLocalPaths).
+        /// </summary>
+        /// <remarks>
+        /// This list will be actively synced back to the build system, i.e. adding or removing items from this list will modify the @(ReferenceCopyLocalPaths) list of the current build.
+        /// </remarks>
         [NotNull]
         protected new IList<string> ReferenceCopyLocalPaths => base.ReferenceCopyLocalPaths;
 
+        /// <summary>
+        /// A list of all the msbuild constants.
+        /// A copy of the contents of the $(DefineConstants).
+        /// </summary>
         [NotNull]
         protected new IList<string> DefineConstants => base.DefineConstants;
 
         void ILogger.LogDebug(string message)
         {
-            LogDebug(message);
+            LogMessage(message, MessageImportance.Low);
         }
 
         void ILogger.LogInfo(string message)
         {
-            LogInfo(message);
+            LogMessage(message, MessageImportance.Normal);
         }
 
         void ILogger.LogWarning(string message, SequencePoint sequencePoint)
---
+++ FodyTools/TypeSystemExtensionMethods.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+using Fody;
+
+using JetBrains.Annotations;
+
+using Mono.Cecil;
+
+namespace FodyTools
+{
+    internal static class TypeSystemExtensionMethods
+    {
+        public static MethodReference ImportMethod<TResult>([NotNull] this BaseModuleWeaver weaver, [NotNull] Expression<Func<TResult>> expression)
+        {
+            GetMethodInfo(expression, out var methodName, out var declaringTypeName, out var argumentTypeNames);
+
+            var typeDefinition = weaver.FindType(declaringTypeName);
+
+            try
+            {
+                var method = typeDefinition.Methods
+                    .Single(m => (m.Name == methodName) && m.Parameters.Select(p => p.ParameterType.FullName).SequenceEqual(argumentTypeNames));
+
+                return weaver.ModuleDefinition.ImportReference(method);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new InvalidOperationException($"Method {methodName} does not exist on type {declaringTypeName}", ex);
+            }
+        }
+
+        public static MethodReference TryImportMethod<TResult>([NotNull] this BaseModuleWeaver weaver, [NotNull] Expression<Func<TResult>> expression)
+        {
+            GetMethodInfo(expression, out var methodName, out var declaringTypeName, out var argumentTypeNames);
+
+            if (!weaver.TryFindType(declaringTypeName, out var typeDefinition))
+                return null;
+
+            var method = typeDefinition.Methods
+                .FirstOrDefault(m => m.Name == methodName && m.Parameters.Select(p => p.ParameterType.FullName).SequenceEqual(argumentTypeNames));
+
+            if (method == null)
+                return null;
+
+            return weaver.ModuleDefinition.ImportReference(method);
+        }
+
+        public static TypeReference ImportType<T>([NotNull] this BaseModuleWeaver weaver)
+        {
+            return weaver.ModuleDefinition.ImportReference(weaver.FindType(typeof(T).FullName));
+        }
+
+        public static TypeReference TryImportType<T>([NotNull] this BaseModuleWeaver weaver)
+        {
+            if (!weaver.TryFindType(typeof(T).FullName, out var typeDefinition))
+                return null;
+
+            return weaver.ModuleDefinition.ImportReference(typeDefinition);
+        }
+
+        private static void GetMethodInfo<TResult>([NotNull] Expression<Func<TResult>> expression, out string methodName, out string declaringTypeName, out string[] argumentTypeNames)
+        {
+            if (!(expression.Body is MethodCallExpression methodCall))
+                throw new ArgumentException("Only method call expression is supported.", nameof(expression));
+
+            methodName = methodCall.Method.Name;
+            declaringTypeName = methodCall.Method.DeclaringType.FullName;
+            argumentTypeNames = methodCall.Arguments.Select(a => a.Type.FullName).ToArray();
+        }
+    }
+}
