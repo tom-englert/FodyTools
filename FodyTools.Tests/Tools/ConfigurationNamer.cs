a5c5702c7edf675d58efa13e6b08f220d62caee0
tom-englert <mail@tom-englert.de>
2019-06-10 18:26:36 +0200

    log more details

--- FodyTools.Tests/Tools/ConfigurationNamer.cs
+++ FodyTools.Tests/Tools/ConfigurationNamer.cs
@@ -5,6 +5,7 @@
 namespace FodyTools.Tests.Tools
 {
     using System;
+    using System.Linq;
 
     using ApprovalTests;
     using ApprovalTests.Namers;
@@ -35,6 +36,8 @@ namespace FodyTools.Tests.Tools
         {
             get
             {
+                Console.WriteLine("Stack: " + Approvals.CurrentCaller.StackTrace);
+                Console.WriteLine("Files: \n  " + string.Join("\n  ", Approvals.CurrentCaller.StackTrace.GetFrames().Select(s => s.GetFileName())));
                 Console.WriteLine("Parser: " + stackTraceParser.parser.ForTestingFramework);
                 Console.WriteLine("Source Path: " + base.SourcePath);
                 return base.SourcePath;
